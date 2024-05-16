b555101b104e748ded775379931a367719488e6e
FrankSkep <f.cornejo.pacheco@gmail.com>
2024-05-15 17:32:17 -0700

    update template, C

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <raylib.h>
+
+#define SCR_WIDTH 1280
+#define SCR_HEIGHT 720
+
+int main()
+{
+    InitWindow(SCR_WIDTH, SCR_HEIGHT, "Simon Game");
+    SetTargetFPS(60);
+
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+        ClearBackground(BLACK);
+        
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
